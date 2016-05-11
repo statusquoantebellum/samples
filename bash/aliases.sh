016dcb95b0cf653a8cb6577ca25314a0c6ca3003
kurtukovi <kurtukovi@aqr.com>
2016-05-11 10:17:05 -0400

    Add hexdump alias; add CtrlP mapping

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -18,5 +18,10 @@ alias du='du --human-readable'
 
 alias psgrep='ps -ef | grep -v grep | grep -e ''UID.*CMD'' -e'
 
+#
+# format x1z means heXadecimal, one byte size, and z - display printable characters
+#
+alias hexdump='od --address-radix x --format x1z --output-duplicates'
+
 alias install='sudo apt-get install'
 
